e8f4521f5388efa33fdcffd1160b1ffdd2378aeb
zoenberger <jeflux@gmail.com>
2015-11-07 14:07:03 -0800

    Moved GitHub api query to github.js Angular service.

--- AngularPluralsight/script.js
+++ AngularPluralsight/script.js
@@ -2,28 +2,49 @@
 
 var app = angular.module('githubViewer', []);
 
-app.controller('MainController', function ($scope, $http) {
+app.controller('MainController', function ($scope, github, $interval, $log, $anchorScroll, $location) {
 
     var onError = function (reason) {
-        $scope.error = "Could not fetch tcolefromportlandhe user";
+        $scope.error = "Could not fetch the user";
     };
 
 
     $scope.search = function (username) {
-        $http.get("https://api.github.com/users/" + username)
-      .then(function (response) {
-          $scope.user = response.data;
-          $http.get($scope.user.repos_url)
-                .then(onRepos, onError);
-      }, onError);
+        $log.info("Searching for " + username);
+        github.getUser(username).then(onUserComplete, onError);
+        if (countdownInterval) {
+            $interval.cancel(countdownInterval);
+            $scope.countdown = null;
+        };
     };
 
-    var onRepos = function(response){
-        $scope.repos = response.data;
+    var onUserComplete = function(data) {
+        $scope.user = data;
+        github.getRepos($scope.user).then(onRepos, onError);
     };
 
-    $scope.username = "angular";
+    var onRepos = function(data) {
+        $scope.repos = data;
+        $location.hash("userDetails");
+        $anchorScroll();
+    };
+
+
+    var decrementCountdown = function () {
+        $scope.countdown -= 1;
+        if($scope.countdown <1) {
+            $scope.search($scope.username);
+        };
+    }
+    var countdownInterval = null;
+
+    var startCountdown = function () {
+        countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown)
+    };
+
+    $scope.username = "Angular";
     $scope.message = "GitHub Viewer";
     $scope.repoSortOrder = "-stargazers_count";
-
+    $scope.countdown = 5;
+    startCountdown();
 });
